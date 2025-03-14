6b6db03ab475fc425fa35e6929ea1ba9c542edd9
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-03-14 16:08:04 +0700

    add ElfinderCustomController

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\ElfinderCustomController;
+use Barryvdh\Elfinder\ElfinderController;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ElfinderController::class, ElfinderCustomController::class);
     }
 
     /**
