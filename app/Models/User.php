c38dfda3e553cc3a9f1865cd178d97163e1ca45b
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-03-14 23:19:09 +0700

    update model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,14 @@ use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Database\Factories\UserFactory;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Gate;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use CrudTrait;
     use HasRoles;
@@ -62,5 +64,40 @@ class User extends Authenticatable
         Gate::define('viewTelescope', function (User $user) {
             return $user->id == 1;
         });
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+
+    public function files(): HasMany
+    {
+        return $this->hasMany(File::class);
+    }
+
+    public function fileShortcuts(): HasMany
+    {
+        return $this->hasMany(FileShortcut::class);
+    }
+
+    public function folderShortcuts(): HasMany
+    {
+        return $this->hasMany(FolderShortcut::class);
     }
 }
